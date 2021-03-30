e27f4de70f5f9cbb1c050691f8e856c5e3db995e
Jacek Kopecky <jacek@jacek.cz>
2021-03-30 20:53:06 +0100

    :art:

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -14,65 +14,73 @@ import { Book } from '../types';
 import AuthorList from './AuthorList';
 import BookListByAuthor from './BookListByAuthor';
 
+const DEFAULT_BOOKS = [
+  {
+    id: '345',
+    title: 'It',
+    author: { fname: 'Steven', lname: 'King' },
+    owned: false,
+    mtime: Date.now(),
+  },
+  {
+    id: '345',
+    title: 'Something',
+    owned: false,
+    mtime: Date.now() - 30 * 1000,
+  },
+  {
+    id: '385',
+    title: 'Light Fantastic',
+    author: { fname: 'Toni', lname: 'Braxton' },
+    owned: false,
+    notes: 'lsdkjfoiwuer',
+    mtime: Date.now() - 30 * 60 * 1000,
+  },
+  {
+    id: '385',
+    title: 'Slight Fantastic',
+    series: 'Discworld',
+    author: { fname: 'Toni', lname: 'Braxton' },
+    notes: `Do esse expetendis mandaremus. Veniam iudicem instituendarum, id nam
+      transferrem, veniam comprehenderit expetendis duis incididunt, magna probant non
+      coniunctione, et noster consequat litteris, anim do ut tempor laborum ita irure
+      quo in dolor admodum a iis quorum summis iis officia. Ita illum et quae, fabulas
+      ea culpa, noster constias voluptate sed mandaremus quo mentitum. Quibusdam
+      summis si laboris comprehenderit.Proident quid export se tempor an hic vidisse
+      consectetur, mandaremus cillum nam offendit tractavissent, in eram possumus,
+      dolor quo cernantur iis senserit praetermissum est possumus, deserunt amet malis
+      probant quem, tempor possumus iudicem. Quo quo velit commodo, te sunt admodum
+      consequat. Elit iis quamquam hic noster. Et incididunt voluptatibus, ut se duis
+      probant ea se in noster quis noster, duis ut laborum ea export.`,
+    owned: false,
+    mtime: Date.now() - 10 * 60 * 60 * 1000,
+  },
+  {
+    id: '385',
+    title: 'Bright Fantastic',
+    series: 'Discworld',
+    author: { fname: 'Toni', lname: 'Braxton' },
+    owned: false,
+    mtime: Date.now() - 10 * 24 * 60 * 60 * 1000,
+  },
+  {
+    id: '367',
+    title: '3',
+    author: { fname: 'Ally', lname: 'Yence' },
+    owned: false,
+    mtime: Date.now() - 50 * 24 * 60 * 60 * 1000,
+  },
+  {
+    id: '367',
+    title: '4',
+    author: { fname: 'Stephen', lname: 'Zing' },
+    owned: false,
+    mtime: Date.now() - 350 * 24 * 60 * 60 * 1000,
+  },
+];
+
 export default function App(): JSX.Element {
-  const [books/* , setBooks */] = React.useState<Book[]>([
-    {
-      id: '345',
-      title: 'It',
-      author: { fname: 'Steven', lname: 'King' },
-      owned: true,
-      mtime: 10,
-    },
-    {
-      id: '385',
-      title: 'Light Fantastic',
-      author: { fname: 'Toni', lname: 'Braxton' },
-      owned: true,
-      notes: 'lsdkjfoiwuer',
-      mtime: 10,
-    },
-    {
-      id: '385',
-      title: 'Slight Fantastic',
-      series: 'Discworld',
-      author: { fname: 'Toni', lname: 'Braxton' },
-      notes: `Do esse expetendis mandaremus. Veniam iudicem instituendarum, id nam
-        transferrem, veniam comprehenderit expetendis duis incididunt, magna probant non
-        coniunctione, et noster consequat litteris, anim do ut tempor laborum ita irure
-        quo in dolor admodum a iis quorum summis iis officia. Ita illum et quae, fabulas
-        ea culpa, noster constias voluptate sed mandaremus quo mentitum. Quibusdam
-        summis si laboris comprehenderit.Proident quid export se tempor an hic vidisse
-        consectetur, mandaremus cillum nam offendit tractavissent, in eram possumus,
-        dolor quo cernantur iis senserit praetermissum est possumus, deserunt amet malis
-        probant quem, tempor possumus iudicem. Quo quo velit commodo, te sunt admodum
-        consequat. Elit iis quamquam hic noster. Et incididunt voluptatibus, ut se duis
-        probant ea se in noster quis noster, duis ut laborum ea export.`,
-      owned: true,
-      mtime: 10,
-    },
-    {
-      id: '385',
-      title: 'Bright Fantastic',
-      series: 'Discworld',
-      author: { fname: 'Toni', lname: 'Braxton' },
-      owned: true,
-      mtime: 10,
-    },
-    {
-      id: '367',
-      title: '3',
-      author: { fname: 'Ally', lname: 'Yence' },
-      owned: true,
-      mtime: 10,
-    },
-    {
-      id: '367',
-      title: '4',
-      author: { fname: 'Stephen', lname: 'Zing' },
-      owned: true,
-      mtime: 10,
-    },
-  ]);
+  const [books/* , setBooks */] = React.useState<Book[]>(DEFAULT_BOOKS);
 
   return (
     <Router>
