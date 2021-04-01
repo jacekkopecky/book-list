9ff20f91140a390b559547d8a56750cfa6a25ac7
Jacek Kopecky <jacek@jacek.cz>
2021-04-01 13:59:54 +0100

    use Typography

--- src/js/components/MainAppBar.tsx
+++ src/js/components/MainAppBar.tsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 export default function MainAppBar(): JSX.Element {
   return (
     <AppBar position="static" style={{ zIndex: 1, position: 'relative' }}>
-      <Toolbar><h1>bananas for books</h1></Toolbar>
-      { /* when changing the title above, also change it in index.html and 404.html */ }
+      <Toolbar>
+        <Typography variant="h5">
+          bananas for books
+          { /* when changing the title above, also change it in index.html and 404.html */ }
+        </Typography>
+      </Toolbar>
     </AppBar>
   );
 }
