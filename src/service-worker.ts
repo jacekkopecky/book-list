29e1abedfd262c692b7283b531ecdc35b3fbf8db
Jacek Kopecky <jacek@jacek.cz>
2023-10-29 15:32:47 +0000

    add logging to service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -6,6 +6,10 @@ import { manifest, version } from '@parcel/service-worker';
 declare let self: ServiceWorkerGlobalScope;
 
 async function install() {
+  console.log({ version });
+  for (const path of manifest) {
+    console.log('adding cache', path);
+  }
   const cache = await caches.open(version);
   await cache.addAll(manifest);
 }
@@ -13,7 +17,14 @@ async function install() {
 async function activate() {
   const keys = await caches.keys();
   await Promise.all(
-    keys.map((key) => key !== version && caches.delete(key)),
+    keys.map((key) => {
+      if (key !== version) {
+        console.log('deleting cache for version', key);
+        return caches.delete(key);
+      } else {
+        return undefined;
+      }
+    }),
   );
 }
 
